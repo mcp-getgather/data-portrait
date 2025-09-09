44b03b5b589d41c7a99c7643adeac1909e2f746d
faisal burhanudin <faisalburhanudin@gmail.com>
2025-09-09 12:13:40 +0700

    remove unused config

--- src/server/config.ts
+++ src/server/config.ts
@@ -6,7 +6,6 @@ dotenv.config();
 export const settings = {
   APP_HOST: process.env.APP_HOST || '',
   GETGATHER_URL: process.env.GETGATHER_URL || '',
-  GETGATHER_API_KEY: process.env.GETGATHER_API_KEY || '',
   MAXMIND_ACCOUNT_ID: process.env.MAXMIND_ACCOUNT_ID || '',
   MAXMIND_LICENSE_KEY: process.env.MAXMIND_LICENSE_KEY || '',
   TOGETHER_API_KEY: process.env.TOGETHER_API_KEY || '',
