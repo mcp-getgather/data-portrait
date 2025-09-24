29bcba4cb8a9d75207ca7ffd92e9aa5b735fa320
faisal burhanudin <faisalburhanudin@gmail.com>
2025-09-24 15:58:24 +0700

    start on background

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ set -e
 
 if [ -n "${TAILSCALE_AUTHKEY}" ]; then
     /app/tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-    /app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname=data-portrait
+    /app/tailscale up --authkey="${TAILSCALE_AUTHKEY}" --hostname=data-portrait &
 else
     echo "TAILSCALE_AUTHKEY not set, skipping Tailscale setup"
 fi
